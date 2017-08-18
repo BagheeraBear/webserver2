1608798bd3c673bcf50f2bc83771404a7255bc04
Ulf Swedin <ulf@swedin.nu>
2017-08-18 11:46:01 +0200

    Initialize repo

---
+++ server.js
@@ -0,0 +1,36 @@
+// hello express
+var express = require('express');
+var app=express();
+var PORT=3000;
+
+var middleware = {
+  requireAuthentication: function(req, res, next){
+    console.log('private route hit');
+    next();
+  },
+  logger: function(req,res, next){
+    console.log('Request: ' + new Date().toString() + ': ' + req.method + ' ' + req.originalUrl);
+    next();
+  }
+}
+
+app.use(middleware.logger);
+
+//route for root-url
+// app.get('/', function(req, res){
+//     res.send('Hello, you are at Express');
+// });
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
+  res.send('About us');
+});
+
+
+// Include Public-folder via PATH [__dirname]
+app.use(express.static(__dirname + '/public'));
+
+console.log(__dirname);
+
+app.listen(PORT, function(){
+  console.log('Express server started on port: ' + PORT  );
+});
